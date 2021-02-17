407ec41d1b4b1b76c8df39438d2ca72fab3ec050
haginus <hagiandrei.ah@gmail.com>
2021-02-17 03:20:56 +0200

    added login guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignedInGuard } from './guards/signed-in.guard';
 import { LoginComponent } from './login/login.component';
 import { StudentSetupComponent } from './setup/student-setup/student-setup.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent, data: { hideDrawer: true, hideToolbar: true } },
-  { path: 'setup/student', component: StudentSetupComponent, data: { hideDrawer: true, title: "Validare" } },
+  { path: 'setup/student', component: StudentSetupComponent, data: { hideDrawer: true, title: "Validare" }, canActivate: [SignedInGuard] },
 ];
 
 @NgModule({
