b7001cc07dcee62bcd61c48ed6ba32000064d0ce
Haginus <hagiandrei.ah@gmail.com>
2021-03-17 15:20:42 +0200

    loading component - added color input

--- src/app/shared/loading/loading.component.ts
+++ src/app/shared/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class LoadingComponent implements OnInit {
 
   constructor() { }
+
+  @Input() color: 'primary' | 'accent' | 'warn' = 'accent';
 
   ngOnInit(): void {
   }
