8fe1dafd00313adeace2bd106032589a4a1bf718
Haginus <hagiandrei.ah@gmail.com>
2021-03-25 19:11:25 +0200

    validation guard for students

--- src/app/student/student-routing.module.ts
+++ src/app/student/student-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotValidatedGuard } from '../guards/not-validated.guard';
+import { ValidatedGuard } from '../guards/validated.guard';
 import { StudentApplicationsComponent } from './pages/applications/applications.component';
 import { StudentPaperComponent } from './pages/paper/paper.component';
 import { StudentSetupComponent } from './pages/setup/student-setup.component';
@@ -7,13 +9,13 @@ import { StundentTeachersGridComponent } from './pages/teachers-grid/teachers-gr
 import { StudentComponent } from './student.component';
 
 const routes: Routes = [
-  { path: '', component: StudentComponent },
-  { path: 'setup', component: StudentSetupComponent, data: { hideDrawer: true, title: "Validare" } },
-  { path: 'teachers', component: StundentTeachersGridComponent, data: { title: "Toți profesorii", mode: "all" } },
-  { path: 'suggested-teachers', component: StundentTeachersGridComponent, data: { title: "Profesori sugerați", mode: "suggested" } },
-  { path: 'applications', component: StudentApplicationsComponent, data: { title: "Cereri" } },
-  { path: 'applications/:state', component: StudentApplicationsComponent, data: { title: "Cereri" } },
-  { path: 'paper', component: StudentPaperComponent, data: { title: "Lucrarea dvs." } }
+  { path: '', component: StudentComponent, canActivate: [ValidatedGuard] },
+  { path: 'setup', component: StudentSetupComponent, data: { hideDrawer: true, title: "Validare" }, canActivate: [NotValidatedGuard] },
+  { path: 'teachers', component: StundentTeachersGridComponent, data: { title: "Toți profesorii", mode: "all" }, canActivate: [ValidatedGuard] },
+  { path: 'suggested-teachers', component: StundentTeachersGridComponent, data: { title: "Profesori sugerați", mode: "suggested" }, canActivate: [ValidatedGuard] },
+  { path: 'applications', component: StudentApplicationsComponent, data: { title: "Cereri" }, canActivate: [ValidatedGuard] },
+  { path: 'applications/:state', component: StudentApplicationsComponent, data: { title: "Cereri" }, canActivate: [ValidatedGuard] },
+  { path: 'paper', component: StudentPaperComponent, data: { title: "Lucrarea dvs." }, canActivate: [ValidatedGuard] }
 
 ];
 
