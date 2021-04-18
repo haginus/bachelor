c59c76e27478a9b1a0177559c021965dbde83028
Haginus <hagiandrei.ah@gmail.com>
2021-04-18 17:50:45 +0300

    warning message when deadline has passed

--- src/app/student/pages/paper/paper.component.ts
+++ src/app/student/pages/paper/paper.component.ts
@@ -1,12 +1,12 @@
 import { devOnlyGuardedExpression } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { combineLatest, of } from 'rxjs';
+import { combineLatest, of, Subscription } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
-import { Paper } from 'src/app/services/auth.service';
+import { AuthService, Paper, SessionSettings } from 'src/app/services/auth.service';
 import { StudentExtraData, StudentService, PaperRequiredDocument } from 'src/app/services/student.service';
-import { PaperDocumentEvent } from '../../../shared/paper-document-list/paper-document-list.component';
+import { AreDocumentsUploaded, PaperDocumentEvent } from '../../../shared/paper-document-list/paper-document-list.component';
 import { StudentExtraDataEditorComponent } from '../../dialogs/student-extra-data-editor/student-extra-data-editor.component';
 
 @Component({
@@ -14,14 +14,21 @@ import { StudentExtraDataEditorComponent } from '../../dialogs/student-extra-dat
   templateUrl: './paper.component.html',
   styleUrls: ['./paper.component.scss']
 })
-export class StudentPaperComponent implements OnInit {
+export class StudentPaperComponent implements OnInit, OnDestroy {
 
-  constructor(private dialog: MatDialog, private student: StudentService, private snackbar: MatSnackBar) { }
+  constructor(private dialog: MatDialog, private student: StudentService, private snackbar: MatSnackBar,
+    private auth: AuthService, private cd: ChangeDetectorRef) { }
 
   paper: Paper = null;
   studentExtraData: StudentExtraData = null;
   isLoadingInitialData: boolean = true;
   isWaitingForDocumentGeneration = false;
+  sessionSettings: SessionSettings;
+  sessionSettingsSub: Subscription;
+  canUploadSecretaryFiles: boolean = true;
+  canUploadPaperFiles: boolean = true;
+  areDocumentsUploaded: boolean;
+  deadlinePassed: boolean = false;
 
   requiredDocuments: PaperRequiredDocument[] = []
 
@@ -32,8 +39,35 @@ export class StudentPaperComponent implements OnInit {
     });
   }
 
+  handleAreDocumentsUploaded(event: AreDocumentsUploaded) {
+    this.areDocumentsUploaded = event.byUploader.student;
+    this._checkSubmissionPeriod();
+    this.deadlinePassed = (!this.canUploadSecretaryFiles && !event.byUploaderCategory.student.secretary_files) || 
+                          (!this.canUploadPaperFiles && !event.byUploaderCategory.student.paper_files)
+    this.cd.detectChanges();
+  }
+
+  private _checkSubmissionPeriod(): void {
+    const today = new Date().setHours(0, 0, 0, 0);
+    const startDateSecretary = new Date(this.sessionSettings.fileSubmissionStartDate).setHours(0, 0, 0, 0);
+    const endDateSecretary = new Date(this.sessionSettings.fileSubmissionEndDate).setHours(0, 0, 0, 0);
+
+    const endDatePaper = new Date(this.sessionSettings.paperSubmissionEndDate).setHours(0, 0, 0, 0);
+
+    this.canUploadSecretaryFiles = startDateSecretary <= today && today <= endDateSecretary;
+    this.canUploadPaperFiles = startDateSecretary <= today && today <= endDatePaper;
+  }
+
   ngOnInit(): void {
     this.getData();
+    this.sessionSettingsSub = this.auth.sessionSettings.subscribe(settings => {
+      this.sessionSettings = settings;
+      this._checkSubmissionPeriod();
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.sessionSettingsSub.unsubscribe();
   }
 
   getData() {
