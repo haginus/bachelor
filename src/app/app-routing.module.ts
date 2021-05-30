143f1117cc283467df8029a375bfdbef2ce4646b
Haginus <hagiandrei.ah@gmail.com>
2021-05-30 16:23:25 +0300

    role guard for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,36 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotSignedInGuard } from './guards/not-signed-in.guard';
 import { SignedInGuard } from './guards/signed-in.guard';
-import { ValidatedGuard } from './guards/validated.guard';
 import { LoginComponent } from './pages/login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ChangePasswordComponent } from './user-components/change-password/change-password.component';
+import { RoleGuard } from './guards/role.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent, data: { hideDrawer: true, hideToolbar: true, animate: false }, canActivate: [NotSignedInGuard] },
-  { path: 'login/token/:token', component: ChangePasswordComponent, data: { hideDrawer: true, hideToolbar: true, animate: false } },
-  { path: 'dashboard', component: DashboardComponent, data: { title: "Dashboard" }, canActivate: [SignedInGuard, ValidatedGuard] },
-  { path: '', pathMatch: "full", component: DashboardComponent, data: { title: "Dashboard" }, canActivate: [SignedInGuard, ValidatedGuard] },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), canActivate: [SignedInGuard] },
-  { path: 'teacher', loadChildren: () => import('./teacher/teacher.module').then(m => m.TeacherModule), canActivate: [SignedInGuard] },
-  { path: 'student', loadChildren: () => import('./student/student.module').then(m => m.StudentModule), canActivate: [SignedInGuard] },
+  { path: 'login',
+    component: LoginComponent,
+    data: { hideDrawer: true, hideToolbar: true, animate: false },
+    canActivate: [NotSignedInGuard]
+  },
+  { path: 'login/token/:token',
+    component: ChangePasswordComponent,
+    data: { hideDrawer: true, hideToolbar: true, animate: false }
+  },
+  { path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+    canActivate: [SignedInGuard, RoleGuard],
+    data: { role: 'admin' }
+  },
+  { path: 'teacher',
+    loadChildren: () => import('./teacher/teacher.module').then(m => m.TeacherModule),
+    canActivate: [SignedInGuard, RoleGuard],
+    data: { role: 'teacher' }
+  },
+  { path: 'student',
+    loadChildren: () => import('./student/student.module').then(m => m.StudentModule),
+    canActivate: [SignedInGuard, RoleGuard],
+    data: { role: 'student' }
+  },
+  { path: '**', redirectTo: '', canActivate: [SignedInGuard, RoleGuard] },
 ];
 
 @NgModule({
