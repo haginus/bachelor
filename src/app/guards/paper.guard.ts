d9226104e8466560ce2899d2f1a36cf00a5089cb
Haginus <hagiandrei.ah@gmail.com>
2021-06-07 01:41:32 +0300

    paper guard

---
+++ src/app/guards/paper.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaperGuard implements CanActivate {
+  constructor(private user: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const hasPaper = route.data.hasPaper == undefined ? true : route.data.hasPaper;
+    return this.user.userData.pipe(
+      map(user => {
+        if(user === undefined) {
+          return false;
+        }
+        else if(!!user.student?.paper != hasPaper) {
+          this.router.navigate([user.type]);
+        }
+        return true;
+      })
+    );
+  }
+}
--- src/app/student/student-routing.module.ts
+++ src/app/student/student-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotValidatedGuard } from '../guards/not-validated.guard';
+import { PaperGuard } from '../guards/paper.guard';
 import { ValidatedGuard } from '../guards/validated.guard';
 import { StudentApplicationsComponent } from './pages/applications/applications.component';
 import { StudentPaperComponent } from './pages/paper/paper.component';
@@ -9,13 +10,47 @@ import { StundentTeachersGridComponent } from './pages/teachers-grid/teachers-gr
 import { StudentComponent } from './student.component';
 
 const routes: Routes = [
-  { path: '', component: StudentComponent, canActivate: [ValidatedGuard] },
-  { path: 'setup', component: StudentSetupComponent, data: { hideDrawer: true, title: "Validare" }, canActivate: [NotValidatedGuard] },
-  { path: 'teachers', component: StundentTeachersGridComponent, data: { title: "Toți profesorii", mode: "all" }, canActivate: [ValidatedGuard] },
-  { path: 'suggested-teachers', component: StundentTeachersGridComponent, data: { title: "Profesori sugerați", mode: "suggested" }, canActivate: [ValidatedGuard] },
-  { path: 'applications', component: StudentApplicationsComponent, data: { title: "Cereri" }, canActivate: [ValidatedGuard] },
-  { path: 'applications/:state', component: StudentApplicationsComponent, data: { title: "Cereri" }, canActivate: [ValidatedGuard] },
-  { path: 'paper', component: StudentPaperComponent, data: { title: "Lucrarea dvs." }, canActivate: [ValidatedGuard] }
+  {
+    path: '',
+    component: StudentComponent,
+    canActivate: [ValidatedGuard]
+  },
+  { 
+    path: 'setup',
+    component: StudentSetupComponent,
+    data: { hideDrawer: true, title: "Validare" },
+    canActivate: [NotValidatedGuard]
+  },
+  { 
+    path: 'teachers',
+    component: StundentTeachersGridComponent,
+    data: { title: "Toți profesorii", mode: "all", hasPaper: false },
+    canActivate: [ValidatedGuard, PaperGuard]
+  },
+  {
+    path: 'suggested-teachers',
+    component: StundentTeachersGridComponent,
+    data: { title: "Profesori sugerați", mode: "suggested", hasPaper: false },
+    canActivate: [ValidatedGuard]
+  },
+  { 
+    path: 'applications',
+    component: StudentApplicationsComponent,
+    data: { title: "Cereri", hasPaper: false },
+    canActivate: [ValidatedGuard]
+  },
+  { 
+    path: 'applications/:state',
+    component: StudentApplicationsComponent,
+    data: { title: "Cereri", hasPaper: false },
+    canActivate: [ValidatedGuard]
+  },
+  { 
+    path: 'paper',
+    component: StudentPaperComponent,
+    data: { title: "Lucrarea dvs." },
+    canActivate: [ValidatedGuard, PaperGuard]
+  }
 
 ];
 
