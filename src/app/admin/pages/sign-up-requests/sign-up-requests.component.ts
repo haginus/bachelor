f1604d3297f0a06f7641b4d57bb6d308cf57616c
Haginus <hagiandrei.ah@gmail.com>
2024-09-19 18:17:14 +0300

    open sign up request from link

--- src/app/admin/pages/sign-up-requests/sign-up-requests.component.ts
+++ src/app/admin/pages/sign-up-requests/sign-up-requests.component.ts
@@ -3,13 +3,14 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatSort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
-import { Subscription } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { SignUpRequestDialogComponent } from '../../dialogs/sign-up-request-dialog/sign-up-request-dialog.component';
 import { AdminService } from '../../../services/admin.service';
 import { DocumentService } from '../../../services/document.service';
 import { SignUpRequest } from '../../../services/auth.service';
 import { DOMAIN_TYPES } from '../../../lib/constants';
 import { rowAnimation } from '../../../row-animations';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up-requests',
@@ -21,38 +22,40 @@ import { rowAnimation } from '../../../row-animations';
 })
 export class SignUpRequestsComponent implements OnInit {
 
-  constructor(private admin: AdminService, private dialog: MatDialog, private document: DocumentService,
-    private snackbar: MatSnackBar) { }
+  constructor(
+    private readonly route: ActivatedRoute,
+    private admin: AdminService,
+    private dialog: MatDialog,
+    private document: DocumentService,
+    private snackbar: MatSnackBar
+  ) {}
 
   @ViewChild('table') table: MatTable<SignUpRequest>;
   @ViewChild(MatSort) sort: MatSort;
 
   dataSource: MatTableDataSource<SignUpRequest>;
   isLoadingResults: boolean = true;
+  isFirstLoad = true;
   displayedColumns: string[] = ['id', 'name', 'specialization', 'group', 'promotion', 'email', 'actions'];
 
   DOMAIN_TYPES = DOMAIN_TYPES;
 
-  private requestSub: Subscription;
-
-  private getRequests(): void {
-    if(this.requestSub) {
-      this.requestSub.unsubscribe();
-    }
+  private async getRequests() {
     this.isLoadingResults = true;
-    this.requestSub = this.admin.getSignUpRequests().subscribe(requests => {
-      this.dataSource.data = requests;
-      this.isLoadingResults = false;
-    });
+    const firstLoadOpenId = this.route.snapshot.queryParamMap.get('id');
+    const requests = await firstValueFrom(this.admin.getSignUpRequests());
+    this.dataSource.data = requests;
+    if(firstLoadOpenId && this.isFirstLoad) {
+      const request = requests.find(request => request.id === +firstLoadOpenId);
+      request && this.openRequest(request);
+    }
+    this.isLoadingResults = false;
+    this.isFirstLoad = false;
   }
 
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource([]);
     this.getRequests();
-  }
-
-  ngOnDestroy() {
-    this.requestSub.unsubscribe();
   }
 
   ngAfterViewInit(): void {
