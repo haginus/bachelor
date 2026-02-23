1da0b7aa15a089c929be585fdc863775614b9d3c
Haginus <hagiandrei.ah@gmail.com>
2026-02-23 03:27:29 +0200

    update app version to 4.0.0

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   apiUrl: "/api",
   production: true,
   captchaKey: "6LddS6YdAAAAAA14-rSoKFwbMp3nAcePSqyk5bqu",
-  appVersion: '3.0.0',
+  appVersion: '4.0.0',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl: "http://localhost:3000",
   captchaKey: "6LcOV6YdAAAAAImFWD5zBvefQiIoDs65j5I4REdL",
-  appVersion: '3.0.0',
+  appVersion: '4.0.0',
 };
 
